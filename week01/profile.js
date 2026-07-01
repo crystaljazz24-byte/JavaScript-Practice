48cbe18913e9e0e88bc6761e99da54280284652f
crystaljazz24-byte <crystaljazz24@gmail.com>
2026-07-01 05:42:20 -0400

    Create developer profile in profile.js
    Fix syntax errors and improve console output formatting.

---
+++ week01/profile.js
@@ -0,0 +1,15 @@
+// Developer Profile
+const firstName= "Crystal":
+const lastName= "Wynn":
+
+let experience = "Learning JavaScript";
+let favoriteLanguage = "JavaScript";
+let portfolioProjects= 0;
+let hired = false;
+console.log("Developer Profile");
+console.log("-----------------------");
+console.log("Name:", firstName, Last Name);
+console.log("Experience:, ", experience);
+console.log(Favorite Language:", favoriteLanguage);
+  console.log("Portfolio Projects:", portfolioProjets);
+console log("Hired:", hired);
